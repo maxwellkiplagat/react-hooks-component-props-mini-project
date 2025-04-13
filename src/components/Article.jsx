e2cb48d643df18a184d78459cc770dbdfb3bf154
maxwellkiplagat <maxwellkiplagat0@gmail.com>
2025-04-13 13:52:08 +0300

    completed lab added time read

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,9 +1,18 @@
 import React from "react";
-function Article({ title, date = "January 1, 1970", preview }){
+function Article({ title, date = "January 1, 1970", preview, minutes }){
+    let emoji = "";
+    if (minutes < 30) {
+      const cups = Math.ceil(minutes / 5);
+      emoji = "☕️".repeat(cups);
+    } else {
+      const boxes = Math.ceil(minutes / 10);
+      emoji = "🍱".repeat(boxes);
+    }
     return(
       <article>
         <h3>{title}</h3>
         <small>{date}</small>
+        <p>{emoji} {minutes} min read</p>
         <p>{preview}</p>
       </article>      
     )
